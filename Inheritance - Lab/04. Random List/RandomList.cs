d2b80691581137c7144b73d5d171fc3db8ce1c27
valyo132 <valyo132@abv.bg>
2022-10-25 21:30:43 +0300

    Inheritance - Lab

---
+++ Inheritance - Lab/04. Random List/RandomList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public string RandomString()
+        {
+            Random rnd = new Random();
+            int index = rnd.Next(Count);
+
+            RemoveAt(index);
+            return this[index].ToString();
+        }
+    }
+}
---
+++ Inheritance - Lab/05. Stack of Strings/StackOfStrings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackOfStrings
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+            => Count == 0;
+
+        public Stack<string> AddRange(params string[] words)
+        {
+            foreach (var item in words)
+                Push(item);
+
+            return this;
+        }
+    }
+}
