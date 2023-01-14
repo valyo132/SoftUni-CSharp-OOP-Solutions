96363f06e92924e879742a6195ae3c4115670758
valyo132 <valyo132@abv.bg>
2023-01-14 19:46:32 +0200

    test commit

---
+++ Encapsulation - Lab/Test-DontOpen/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Test_DontOpen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Why did you opened that?!");
+            Console.WriteLine("Just testing something.");
+        }
+    }
+}
